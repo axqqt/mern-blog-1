6c587d4aeeb5e9d77f7ff6cb76062d56b0074ced
DulranSam <dulransamarasinghe4@gmail.com>
2024-06-18 03:36:05 +0530

    what

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
 
 dotenv.config();
 
@@ -16,11 +16,10 @@ mongoose
     console.log('MongoDb is connected');
   })
   .catch((err) => {
-    console.log(err);
+    console.error('MongoDb connection error:', err);
   });
 
 const __dirname = path.resolve();
-
 const app = express();
 
 app.use(express.json());
